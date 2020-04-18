8a1e5cd15a9d8378bd402bf36bf6e2197a8e88cb
Martin Barria Joost <mauricio.joost@litermi.com>
2020-04-18 12:33:51 -0600

    fix indentation

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ const path = require('path');
 const express = require('express');
 
 
-const SocketIO  = require('socket.io');  
-const io = SocketIO(server);
 const app = express();
-
-//websockets
-io.on('connection', ()=>{
-    console.log("Conectado");
-    
-})
-
 
 //settings 
 app.set('port', process.env.PORT || 3000);
 
+
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 //start server
 const server = app.listen(app.get('port', ), ()=>{
     console.log('server on port', app.get('port'));
+})
+
+
+
+//websockets
+const SocketIO  = require('socket.io');  
+const io = SocketIO(server);
+
+io.on('connection', ()=>{
+    console.log("Conectado");
+    
 })
