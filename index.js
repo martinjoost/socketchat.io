3c6cc072ebc7db9525f3ccd5ab1505a860f5b01c
Martin Barria Joost <mauricio.joost@litermi.com>
2020-04-18 11:12:07 -0600

    folders

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
 //settings 
 app.set('port', process.env.PORT || 3000);
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 //start server
 app.listen(app.get('port', ), ()=>{
