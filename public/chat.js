b840654c829cd1616572968e009f94ab2bff0e4b
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-20 21:07:23 -0600

    fix username

--- public/chat.js
+++ public/chat.js
@@ -3,10 +3,14 @@ const socket = io()
 
 //DOM ELEMENTS
 let message = document.getElementById('message');
+let username = document.getElementById('username');
 let btn = document.getElementById('send');
 let output = document.getElementById('output');
 let actions = document.getElementById('actions');
 
 btn.addEventListener('click', function(){
-    console.log("Click");
+    console.log ( 
+        username.value,
+        message.value,
+     );
 })
