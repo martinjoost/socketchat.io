c86ee7ec0888e936e3e7a47db570b60ced83ab3e
Mauricio Joost <mauricio.joost@litermi.com>
2020-04-20 21:03:20 -0600

    dom elements

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,13 @@
 const socket = io()
+
+
+//DOM ELEMENTS
+let message = document.getElementById('message');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+let actions = document.getElementById('actions');
+
+btn.addEventListener('click', function(){
+    console.log("Click");
+    
+})
